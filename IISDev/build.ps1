4caf0cae861c9dc96c3fd120d71ebf22de00ca70
Darrell Tunnell <darrell.tunnell@googlemail.com>
2018-05-28 14:33:18 +0100

    Added build scripts.

---
+++ IISDev/build.ps1
@@ -0,0 +1,7 @@
+Write-Host Building registry binary and image
+
+$version = $env:GitVersion_NuGetVersionV2
+Write-Host "Version is $version"
+
+docker build -t iisdev .
+docker tag iisdev:latest iisdev:$version
